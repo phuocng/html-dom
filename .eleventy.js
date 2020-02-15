a0f774e5a3fe1db77509c76a4d67cd9e49a8c203
Phuoc Nguyen <me@phuoc.ng>
2020-02-15 16:39:32 +0700

    Basic eleventy setup

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+    return {
+        templateFormats: [
+            "md",
+            "njk",
+            "html",
+            "liquid"
+        ],
+        markdownTemplateEngine: "liquid",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+    };
+};
