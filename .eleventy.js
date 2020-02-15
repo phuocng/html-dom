9d1e6f7120793130a9fe011a62724978491a93de
Phuoc Nguyen <me@phuoc.ng>
2020-02-15 17:01:24 +0700

    Add tailwind CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,5 @@
 module.exports = function(eleventyConfig) {
     return {
-        dir: {
-            input: "src/site",
-            includes: "_includes",
-            output: "dist",
-        },
         templateFormats: ["md", "njk"],
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk",
