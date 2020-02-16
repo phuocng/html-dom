585024a2d24a4b448cbd19110824a2652235f0b7
Phuoc Nguyen <me@phuoc.ng>
2020-02-16 19:03:12 +0700

    Add link to homepage to layout

--- client/pages/Layout.tsx
+++ client/pages/Layout.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import Filter from '../components/Filter';
 import Footer from './Footer';
@@ -12,8 +13,9 @@ const Layout: React.FC<{}> = ({ children }) => {
         <>
             <div className='flex-1 px-2 lg:px-0'>
                 <div className="ml-auto mr-auto max-w-3xl mb-8">
-                    <div className='flex justify-end'>
-                        <div className='my-4 w-1/2'>
+                    <div className='flex items-center justify-between my-8'>
+                        <Link to='/' className='bg-black p-1 rounded text-3xl font-light text-white'>&lt;/dom&gt;</Link>
+                        <div className='w-1/2'>
                             <Filter />
                         </div>
                     </div>
