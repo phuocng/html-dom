5f9e8263ab51ae34fbbb0f47934020c6c008cac9
Phuoc Nguyen <me@phuoc.ng>
2020-02-16 21:43:57 +0700

    Clone an element

--- client/constants/TaskMetaList.ts
+++ client/constants/TaskMetaList.ts
@@ -10,6 +10,7 @@ const TaskMetaList: TaskMeta[] = [
     { taskType: TaskType.AddClass, title: 'Add or remove class from an element', slug: 'add-or-remove-class-from-an-element' },
     { taskType: TaskType.Match, title: 'Check an element against a selector', slug: 'check-an-element-against-a-selector' },
     { taskType: TaskType.HasClass, title: 'Check if an element has given class', slug: 'check-if-an-element-has-given-class' },
+    { taskType: TaskType.Clone, title: 'Clone an element', slug: 'clone-an-element' },
     { taskType: TaskType.Create, title: 'Create an element', slug: 'create-an-element' },
     { taskType: TaskType.Ready, title: 'Execute code when the document is ready', slug: 'execute-code-when-the-document-is-ready' },
     { taskType: TaskType.Closest, title: 'Get the closest element by given selector', slug: 'get-the-closest-element-by-given-selector' },
