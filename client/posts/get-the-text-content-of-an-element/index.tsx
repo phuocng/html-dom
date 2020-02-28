5696dede3f27cb5a387071d10fd6b79bf075c39a
Phuoc Nguyen <me@phuoc.ng>
2020-02-28 22:43:56 +0700

    Fix the textContent() usage

--- client/posts/get-the-text-content-of-an-element/index.tsx
+++ client/posts/get-the-text-content-of-an-element/index.tsx
@@ -12,7 +12,7 @@ Returns the raw text content of the \`ele\` element and its children.
 All the HTML tags are excluded.
 
 ~~~ javascript
-const text = ele.textContent();
+const text = ele.textContent;
 ~~~
 `}
 />
