8124155889970e94a1545c361873ccf994832f0e
Phuoc Nguyen <me@phuoc.ng>
2020-03-08 18:06:12 +0700

    Sort a table by clicking its headers

--- client/posts/loop-over-a-nodelist/index.tsx
+++ client/posts/loop-over-a-nodelist/index.tsx
@@ -55,6 +55,7 @@ elements.forEach(function(ele) {
         'resize-columns-of-a-table',
         'select-an-element-or-list-of-elements',
         'select-the-children-of-an-element',
+        'sort-a-table-by-clicking-its-headers',
     ]}
 />
 </>
