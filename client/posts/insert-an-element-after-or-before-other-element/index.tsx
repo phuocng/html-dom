54debf0c558a508b22e2f5bfca45c4d4acb41954
Phuoc Nguyen <me@phuoc.ng>
2020-03-16 20:55:37 +0700

    Add insertAdjacentElement example

--- client/posts/insert-an-element-after-or-before-other-element/index.tsx
+++ client/posts/insert-an-element-after-or-before-other-element/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import Markdown from '../../components/Markdown';
 import RelatedPosts from '../../components/RelatedPosts';
@@ -6,6 +7,9 @@ import RelatedPosts from '../../components/RelatedPosts';
 export default () => {
     return (
 <>
+<Helmet>
+    <meta name='keywords' content='insertAdjacentElement, insertBefore' />
+</Helmet>
 <Markdown
     content={`
 ## Insert after
@@ -14,6 +18,9 @@ Insert the \`ele\` element after the \`refEle\` element:
 
 ~~~ javascript
 refEle.parentNode.insertBefore(ele, refEle.nextSibling);
+
+// Or
+refEle.insertAdjacentElement('afterend', ele);
 ~~~
 
 ## Insert before
@@ -22,6 +29,9 @@ Insert the \`ele\` element before the \`refEle\` element:
 
 ~~~ javascript
 refEle.parentNode.insertBefore(ele, refEle);
+
+// Or
+refEle.insertAdjacentElement('beforebegin', ele);
 ~~~
 `}
 />
