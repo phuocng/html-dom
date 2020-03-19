8206b4eeb12bb9905cd098fa5d73dbb82c67efaf
Phuoc Nguyen <me@phuoc.ng>
2020-03-19 23:02:44 +0700

    Add subscribe form

--- client/pages/Layout.tsx
+++ client/pages/Layout.tsx
@@ -15,7 +15,7 @@ const Layout: React.FC<{}> = ({ children }) => {
         <>
             <div className='flex-1 px-2 lg:px-0'>
                 <div className="ml-auto mr-auto max-w-3xl mb-8">
-                    <div className='flex items-center justify-between my-8'>
+                    <div className='flex items-center justify-between py-8'>
                         <Link to='/' className='bg-black p-1 rounded text-2xl sm:text-3xl font-light text-white'>&lt;/dom&gt;</Link>
                         <div className='w-2/3 sm:w-1/2'>
                             <Filter />
