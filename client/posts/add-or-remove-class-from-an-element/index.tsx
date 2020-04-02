310cfb40cdb81f82577dcbda07d2781900f79aea
xhh97 <xuhhemail@163.com>
2020-04-03 04:03:22 +0800

    add-or-remove-class-from-an-element 文章翻译

--- client/posts/add-or-remove-class-from-an-element/index.tsx
+++ client/posts/add-or-remove-class-from-an-element/index.tsx
@@ -8,29 +8,29 @@ export default () => {
 <>
 <Markdown
     content={`
-## 向一个元素添加 class 属性
+## 向一个元素添加 class 属性值
 
-Note that multiple parameters for the \`add()\` [isn't supported](https://caniuse.com/#feat=classlist) in IE 11.
+\`add()\` 方法的多个参数在 IE 11 中[不被支持](https://caniuse.com/#feat=mdn-api_element_classlist_add_and_remove_multiple_arguments "add() 方法的多个参数在 IE 11 中不被支持")。
 
 ~~~ javascript
 ele.classList.add('class-name');
 
-// Add multiple classes (Not supported in IE 11)
+// 添加多个类 (在 IE 11 中不被支持)
 ele.classList.add('another', 'class', 'name');
 ~~~
 
-## Remove a class from an element
+## 删除元素多个 class 属性值
 
-Note that multiple parameters for the \`remove()\` [isn't supported](https://caniuse.com/#feat=classlist) in IE 11.
+\`remove()\` 方法的多个参数在 IE 11 中[不被支持](https://caniuse.com/#feat=mdn-api_element_classlist_add_and_remove_multiple_arguments "remove() 方法的多个参数在 IE 11 中不被支持")。
 
 ~~~ javascript
 ele.classList.remove('class-name');
 
-// Remove multiple classes (Not supported in IE 11)
+// 删除多个类 (在 IE 11 中不被支持)
 ele.classList.remove('another', 'class', 'name');
 ~~~
 
-## Toggle a class
+## 切换一个 class 属性值
 
 ~~~ javascript
 ele.classList.toggle('class-name');
