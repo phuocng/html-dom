d989a1281808f6af307c11c7afaccea3f9d28b4b
Phuoc Nguyen <me@phuoc.ng>
2020-04-02 10:14:48 +0700

    Calculate the mouse position relative to an element

---
+++ client/posts/calculate-the-mouse-position-relative-to-an-element/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import Markdown from '../../components/Markdown';
+import RelatedPosts from '../../components/RelatedPosts';
+
+export default () => {
+    return (
+<>
+<Helmet>
+    <meta name='keywords' content='addEventListener, clientX, clientY, getBoundingClientRect, mouse position' />
+</Helmet>
+<Markdown
+    content={`
+The following code calculates the mouse position relative to the clicked element:
+
+~~~ javascript
+ele.addEventListener('mousedown', function(e) {
+    // Get the target
+    const target = e.target;
+
+    // Get the bounding rectangle of target
+    const rect = target.getBoundingClientRect();
+
+    // Mouse position
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+});
+~~~
+`}
+/>
+<RelatedPosts
+    slugs={[
+        'attach-or-detach-an-event-handler',
+        'drag-and-drop-element-in-a-list',
+        'get-the-position-of-an-element-relative-to-another',
+        'get-the-position-of-an-element-relative-to-the-document',
+        'make-a-draggable-element',
+    ]}
+/>
+</>
+    );
+};
