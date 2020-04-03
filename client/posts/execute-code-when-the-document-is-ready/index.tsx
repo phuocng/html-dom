148c01d26df9f241860abf5f20078877d3ce1680
Phuoc Nguyen <me@phuoc.ng>
2020-04-03 21:24:45 +0700

    Fix the document ready post

--- client/posts/execute-code-when-the-document-is-ready/index.tsx
+++ client/posts/execute-code-when-the-document-is-ready/index.tsx
@@ -7,7 +7,18 @@ export default () => {
 <Markdown
     content={`
 ~~~ javascript
-document.addEventListener('DOMContentLoaded', function(){
+const ready = function(cb) {
+    // Check if the \`document\` is loaded completely
+    (document.readyState === 'loading')
+        ? document.addEventListener('DOMContentLoaded', function(e) {
+            cb();
+        });
+        : cb()
+};
+
+// Usage
+ready(function() {
+    // Do something when the document is ready
     ...
 });
 ~~~
