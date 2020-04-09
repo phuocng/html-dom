d7a66517c293b598390af58eb861dc192a3c1f88
surajhell88 <surajhell88@gmail.com>
2020-04-09 14:01:26 +0530

    Reshuffling some code to separate logic for home and page routes

--- client/pages/Layout.tsx
+++ client/pages/Layout.tsx
@@ -11,9 +11,10 @@ const Layout: React.FC<{}> = ({ children }) => {
     const { pathname } = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, oldYOffsetValue);
-        // attach scroll event listener only when at home page
         if (pathname === '/') {
+            // scroll to last saved vertical scroll location
+            // when user navigates to home
+            window.scrollTo(0, oldYOffsetValue);
             const scrollEvent = throttle(() => {
                 // save last known vertical scroll position
                 oldYOffsetValue = window.scrollY || window.pageYOffset;
@@ -21,9 +22,11 @@ const Layout: React.FC<{}> = ({ children }) => {
             window.addEventListener('scroll', scrollEvent);
             return function () {
                 // detach scroll listener
-                oldYOffsetValue = 0;
                 window.removeEventListener('scroll', scrollEvent);
             };
+        } else {
+            // scroll to top when user navigates to any post
+            window.scrollTo(0, 0);
         }
     }, [pathname]);
 
