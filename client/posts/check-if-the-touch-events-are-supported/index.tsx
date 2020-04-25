bfbab13673e1855ce9e5d897ba5e594d84054d44
Phuoc Nguyen <me@phuoc.ng>
2020-04-25 08:22:53 +0700

    Check if the touch events are supported

---
+++ client/posts/check-if-the-touch-events-are-supported/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import Markdown from '../../components/Markdown';
+
+export default () => {
+    return (
+<>
+<Helmet>
+    <meta name='keywords' content='check touch events supported, DocumentTouch' />
+</Helmet>
+<Markdown
+    content={`
+Check if the current browser supports the touch events:
+
+~~~ javascript
+const touchSupported = 'ontouchstart' in window || 
+    (window.DocumentTouch && document instanceof DocumentTouch);
+~~~
+`}
+/>
+</>
+    );
+};
