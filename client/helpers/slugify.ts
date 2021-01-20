a5e1285fa53d6e3a45c159c64063f53fe536b234
Phuoc Nguyen <me@phuoc.ng>
2021-01-20 15:45:37 +0700

    Remove the old packages

--- client/helpers/slugify.ts
+++
@@ -1,9 +0,0 @@
-const slugify = (item: string) => item.toLowerCase().split(' ').join('-');
-
-const capitalizeFirstChar = (input: string) => `${input.substr(0, 1).toUpperCase()}${input.substr(1)}`;
-const unslugify = (slug: string) => capitalizeFirstChar(slug.toLowerCase().split('-').join(' '));
-
-export {
-    slugify,
-    unslugify,
-};
