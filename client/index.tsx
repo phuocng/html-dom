94f53411eca9b918fa0fcad56e73e4693d130e00
Phuoc Nguyen <me@phuoc.ng>
2021-05-05 08:58:07 +0700

    Remove React components

--- client/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { hydrate, render } from 'react-dom';
-
-import App from './App';
-
-const rootElement = document.getElementById('root');
-rootElement.hasChildNodes()
-    ? hydrate(<App />, rootElement)
-    : render(<App />, rootElement);
