3960d9009c4bb8a0232fd74bb86c2ea9766f2020
Phuoc Nguyen <me@phuoc.ng>
2021-05-05 08:58:37 +0700

    Copy demo

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('assets');
     eleventyConfig.addPassthroughCopy('robots.txt');
     eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('demo');
 
     eleventyConfig.addWatchTarget('./css/');
 
