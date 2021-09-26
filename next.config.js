f076c99070ab5e97b47e8111c9a3a19de219eccc
Phuoc Nguyen <me@phuoc.ng>
2021-09-26 15:03:29 +0700

    Remove old files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/,
 });
@@ -8,7 +6,4 @@ module.exports = withMDX({
     // `true` will transform `/about` to `/about/index.html`
     trailingSlash: false,
     pageExtensions: ['js', 'jsx', 'tsx', 'mdx'],
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        return config;
-    },
 });
